a0572b7c7b35504a32181acb4a228e942a405d9c
Elman <134933773+Shternoa@users.noreply.github.com>
2023-07-07 14:31:45 +0300

    Find a string ( 0_0 )

--- Tasks.py
+++ Tasks.py
@@ -379,3 +379,17 @@
 # print(mutate_string('abracadabra', 5, 'k'))
 
 # --- Mutations --- #
+
+# --- Find a string --- #
+
+# def count_substring(string, sub_string):
+#     count = 0
+#     for i in range(0, len(string)):
+#         if string[i:i + len(sub_string)] == sub_string:
+#             count +=1
+#     return count
+#
+#
+# print(count_substring('ABCDCDC', 'CDC'))
+
+# --- Find a string --- #
